0751e15809528ef56515eb02cb10edd6a61c26ca
Joey Feng <jfeng702@gmail.com>
2018-07-23 15:51:27 -0700

    update

--- prod/users/test/validation_test.js
+++ prod/users/test/validation_test.js
@@ -1,2 +1,13 @@
 const assert = require('assert');
 const User = require('../src/user');
+
+describe('Validating records', () => {
+  it('requires a user name', () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    console.log(validationResult);
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name is required');
+  });
+});
